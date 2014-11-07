13a2fc481e69e2b67e28fd47fee23ac2bf5cb6aa
Jacob Schoo <jacob.schoo@gmail.com>
2014-11-07 15:38:09 +0000

    Seed data added

--- db/migrate/20141107135137_create_photos.rb
+++ db/migrate/20141107135137_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
       t.string :name
       t.text :description
+      t.text :photo
       t.integer :user_id
       t.integer :album_id
       t.integer :location_id
