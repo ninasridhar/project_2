61df3ac0d70069d00195c57024bf7a54fc7d12ba
ninasridhar <nina.sridhar.1@gmail.com>
2014-11-07 11:07:10 +0000

    initial commit to get project onto git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = 'ee5903d6be7eebfcd80566166fe442ae22efdfe01af59a7fbbf6d98b4f806a115e072dc4ac37d1377f8a9f88121557f156fee301e44e538f220f0cb5d7a8f49b'
