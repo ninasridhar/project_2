1cc91840dd445b6b9a41abadc449f811636bad87
Jacob Schoo <jacob.schoo@gmail.com>
2014-11-09 15:56:00 +0000

    added some seed data

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :album_id, :category_id, :description, :location_id, :name, :user_id, :album_id, :location_id, :category_id, :photo_link
+  attr_accessible :album_id, :category_id, :description, :location_id, :name, :user_id, :album_id, :location_id, :category_id, :photo_link, :upload_image
 
   has_many :votes
   has_many :comments, as: :multi
@@ -9,4 +9,6 @@ class Photo < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :albums
 
+
+  mount_uploader :upload_image, UploadImageUploader
 end
