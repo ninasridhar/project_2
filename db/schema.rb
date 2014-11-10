4794b5e77e120e22e768288cc507258c553ea5b4
ninasridhar <nina.sridhar.1@gmail.com>
2014-11-10 13:19:28 +0000

    search bar now works and added some bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141109161213) do
+ActiveRecord::Schema.define(:version => 20141110112656) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
