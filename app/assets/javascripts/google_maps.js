69ef835f5bec40cd1ef43de61f2e5a7ca344b8f2
iferunsewe@gmail.com <iferunsewe@users.noreply.github.com>
2014-11-10 11:56:28 +0000

    google maps on index page

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,9 +1,10 @@
-var mapOptions, canvas, map, mapApp;
+var mapOptions, canvas, map, mapApp, geocoder;
 mapApp = {};
 
 mapApp.initialize = function(){
   canvas = document.getElementById('map_canvas');
   var latLng = new google.maps.LatLng(51.508742, -0.120850)
+  // geocoder = new google.maps.Geocoder();
   mapOptions = {
     zoom: 8,
     center:latLng,
@@ -11,5 +12,9 @@ mapApp.initialize = function(){
   };
   map = new google.maps.Map(canvas, mapOptions);
 }
+
+// mapApp.codeAddress = function(){
+//   var Address = document.getElementById('city').value;
+// }
 
 $(mapApp.initialize);
