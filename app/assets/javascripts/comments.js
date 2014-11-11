2981717cf953e483e6c8933aa299494aca5b9d61
ninasridhar <nina.sridhar.1@gmail.com>
2014-11-11 16:06:25 +0000

    comments now works for both albums and photos

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,42 @@
+ajaxComments = {};
+
+var itemId = parseInt(window.location.href.split('/').pop());
+var user  = $('#username').val();
+var multitype = window.location.href.split('/')[3];
+
+ajaxComments.getComments = function(event){
+  $.ajax({
+    url: '/comments',
+    type: 'GET',
+    dataType: 'json'
+  }).success(function(data){
+    $('.comment').html('');
+    $.each(data, function(index, item){
+      if ((item.multi_type === multitype)&(item.multi_id === itemId)){
+        $('.comment').append("<tr><td><b>"+ item.username +"</b></td></tr><tr><td>"+ item.text +"</td></tr>")
+      } 
+    }) 
+  })
+}
+
+ajaxComments.postComment = function(event){
+  event.preventDefault();
+  var user  = $('#username').val();
+  var words = $('textarea#comment').val();
+  console.log(user)
+  $.ajax({
+    url: '/comments',
+    method: 'POST',
+    data: {comment: {username: user, multi_id: itemId, multi_type: multitype, text: words}},
+    dataType: 'json'
+  }).success(function(data){
+    console.log(data);
+    ajaxComments.getComments();
+  })
+}
+
+$(document).ready(function(){
+  $('#pencil').on('click', ajaxComments.postComment)
+  ajaxComments.getComments();
+})
+ 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :multi_id, :multi_type, :user_id
+  attr_accessible :multi_id, :multi_type, :username, :text
 
   belongs_to :multi, polymorphic: true
 end
