920117a7d4baff68a0296a9f2ea60023d5cd01a2
Jacob Schoo <jacob.schoo@gmail.com>
2014-11-11 15:53:56 +0000

    small color changes and new logo

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
-    $(".lupe").click(function () {
+    $(".buttonsmall").click(function () {
     	console.log('YO!')
     	$(".nav").toggle("fast");
+        $(".buttonsmall").slideToggle("fast");
+    });
+
+    $(".lupe").click(function () {
+    	console.log('YO!')
+    	$(".nav").toggle("slow");
         $(".searchbox").slideToggle("fast");
     });
+
     $('.drawers').find('h5').click(function(){
     $(this).next().slideToggle();
     $(".drawers p").not($(this).next()).slideUp();
 	});
+    
+    $(".lupe").mouseenter(function(){
+    $("<audio></audio>").attr({ 
+        'src':'audios/snap.wav', 
+        'volume':0.4,
+        'autoplay':'autoplay'
+    }).appendTo("body");
+});
 });
 
 
-
-
-
-var playAudio = $("audio")[0];
-$(".icon")
-	.mouseenter(function() {
-		playAudio.play();
-	});
 
