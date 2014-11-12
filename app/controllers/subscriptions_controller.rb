9b347e771b3ff1a4dff54ef396d981ed5ba9261c
ninasridhar <nina.sridhar.1@gmail.com>
2014-11-12 16:15:11 +0000

    can subscribe but cant unsub

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,41 +1,82 @@
 class SubscriptionsController < ApplicationController
-  before_filter :set_subscription, only: [:show, :edit, :update, :destroy]
-
+  # GET /tags
+  # GET /tags.json
   def index
     @subscriptions = Subscription.all
-    respond_with(@subscriptions)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @subscriptions }
+    end
   end
 
+  # GET /tags/1
+  # GET /tags/1.json
   def show
-    respond_with(@subscription)
+    @subscription = Subscription.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @subscription }
+    end
   end
 
+  # GET /tags/new
+  # GET /tags/new.json
   def new
     @subscription = Subscription.new
-    respond_with(@subscription)
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @subscription }
+    end
   end
 
+  # GET /tags/1/edit
   def edit
+    @subscription = Subscription.find(params[:id])
   end
 
+  # POST /tags
+  # POST /tags.json
   def create
     @subscription = Subscription.new(params[:subscription])
-    @subscription.save
-    respond_with(@subscription)
-  end
-
-  def update
-    @subscription.update_attributes(params[:subscription])
-    respond_with(@subscription)
-  end
-
-  def destroy
-    @subscription.destroy
-    respond_with(@subscription)
-  end
-
-  private
-    def set_subscription
-      @subscription = Subscription.find(params[:id])
+    respond_to do |format|
+      if @subscription.save
+        format.html { redirect_to @subscription, notice: 'subscription was successfully created.' }
+        format.json { render json: @subscription, status: :created, location: @subscription }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @subscription.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  # PUT /tags/1
+  # PUT /tags/1.json
+  def update
+    @subscription = Subscription.find(params[:id])
+
+    respond_to do |format|
+      if @subscription.update_attributes(params[:subscription])
+        format.html { redirect_to @subscription, notice: 'subscription was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @subscription.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tags/1
+  # DELETE /tags/1.json
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    @subscription.destroy
+
+    respond_to do |format|
+      format.html { redirect_to subscriptions_url }
+      format.json { head :no_content }
+    end
+  end
 end
